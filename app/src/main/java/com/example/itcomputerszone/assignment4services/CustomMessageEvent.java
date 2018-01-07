ba68c27402b5935c2031815c87ee062e05919928
shahbaz8 <muhammadshahbaz9934@gmail.com>
2018-01-07 17:35:27 +0500

    Initial commit

---
+++ app/src/main/java/com/example/itcomputerszone/assignment4services/CustomMessageEvent.java
@@ -0,0 +1,18 @@
+package com.example.itcomputerszone.assignment4services;
+
+/**
+ * Created by IT Computer's Zone on 1/7/2018.
+ */
+
+public class CustomMessageEvent {
+
+    private int limit;
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+}
---
+++ app/src/main/java/com/example/itcomputerszone/assignment4services/MyService.java
@@ -0,0 +1,45 @@
+package com.example.itcomputerszone.assignment4services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+
+public class MyService extends Service {
+    private static final String TAG = "MTAG";
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        //throw new UnsupportedOperationException("Not yet implemented");
+
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // return super.onStartCommand(intent, flags, startId);
+
+        Log.d(TAG, "onStartCommand: Service Started");
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+                Log.d(TAG, "onStartCommand: Service Started" + i);
+            } catch (InterruptedException e) {
+
+            }
+        }
+        return START_NOT_STICKY;
+    }
+
+}
